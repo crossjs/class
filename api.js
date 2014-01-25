e440e667920e14d52b859342b81c8528cdd28fe8
crossjs <one@ffully.com>
2014-01-25 22:29:08 +0800

    init

---
+++ api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Class",
+        "Singleton",
+        "Super"
+    ],
+    "modules": [
+        "Class"
+    ],
+    "allModules": [
+        {
+            "displayName": "Class",
+            "name": "Class",
+            "description": "类"
+        }
+    ]
+} };
+});
